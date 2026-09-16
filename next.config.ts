7f12b33a46174b434381a0d0bb78aba6893f86dd
ProgJosh <Joshua27emmanuel30@gmail.com>
2026-09-16 21:11:48 +0800

    Fix next config and update pages workflow

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    output: 'export', // Forces Next.js to compile static HTML/CSS/JS
+  basePath: '/emmanuel-velo-portfolio', // Matches your GitHub repository name
+  images: {
+    unoptimized: true, // Required because Next.js image optimization needs a server
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
